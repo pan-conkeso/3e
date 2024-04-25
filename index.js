4b6a0154fa3e794ee6a9bbc4cdd71ba500ad2259
pan-conkeso <118994579+pan-conkeso@users.noreply.github.com>
2024-04-24 22:32:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,133 @@
+const tareas = [{
+
+        id: 1,
+        nombre: "La orgnización y los elementos que la componen",
+        fecha: "-/-/-",
+        tarea: `<iframe href="https://samuelorellana123.github.io/" width="20" height="20" name="organización y elementos que la componen></iframe>`,
+    },
+    {
+
+        id: 2,
+        nombre: "Adam Smith",
+        fecha: "18/04/2024",
+        desc: "Procesos Administrativos",
+        tarea: `<div  width: 100%; height: 0; 
+    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); 
+    border-radius: 8px; will-change: transform;">
+     <iframe width: 20%; height: 20%; border: none; padding: 0;margin: 0;"
+       src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGCuAMvHD8&#x2F;xZ5FoJjuO2ukDTw7bi1QfQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+     </iframe>
+   </div>
+   <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGCuAMvHD8&#x2F;xZ5FoJjuO2ukDTw7bi1QfQ&#x2F;view?utm_content=DAGCuAMvHD8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>
+   `,
+    },
+
+    {
+
+        id: 3,
+        nombre: "Diagrama Procesos Administrativos",
+        fecha: "23/04/2024",
+        desc: "Procesos Administrativos",
+        tarea: `<div width: 20%;
+        padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16);
+        border-radius: 8px; will-change: transform;">
+         <iframe width: 20%; height: 20%; border: none; padding: 0;margin: 0;"
+           src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDQJcwKvE&#x2F;misDPtJ6z38R1miY0XPuQA&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+         </iframe>
+       </div>
+       <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDQJcwKvE&#x2F;misDPtJ6z38R1miY0XPuQA&#x2F;view?utm_content=DAGDQJcwKvE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>`,
+    },
+
+    {
+        id: 4,
+        nombre: "Autoritarismo",
+        fecha: "23/04/24",
+        desc: "Educación Ciudadana",
+        tarea: `<div width: 20%; height: 0;
+      padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+      border-radius: 8px; will-change: transform;">
+       <iframe width: 20%; height: 20%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+         src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGB8xZ8oBI&#x2F;jn50MKEJwt8vnnmOSKFPcA&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+       </iframe>
+     </div>
+     <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGB8xZ8oBI&#x2F;jn50MKEJwt8vnnmOSKFPcA&#x2F;view?utm_content=DAGB8xZ8oBI&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>`,
+
+    },
+    {
+
+        id: 5,
+        nombre: "Libro de compras",
+        fecha: "24/04/2024",
+        desc: "Utilización de información contable",
+        tarea: `<div  width: 20%; height: 0; 
+    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); 
+    border-radius: 8px; will-change: transform;">
+     <iframe width: 20%; height: 20%; border: none; padding: 0;margin: 0;"
+       src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGCOHJqgBE&#x2F;PAFmKzaaGQ0BzTrOcxyceQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+     </iframe>
+   </div>
+   <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGCOHJqgBE&#x2F;PAFmKzaaGQ0BzTrOcxyceQ&#x2F;view?utm_content=DAGCOHJqgBE&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>
+   `,
+    },
+
+
+    {
+
+        id: 6,
+        nombre: "Sociedad en comandita simple",
+        fecha: "24/04/2024",
+        desc: "Gestión comercial y tributaria",
+        tarea: `<div width: 20%; height: 0;
+        padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+        border-radius: 8px; will-change: transform;">
+         <iframe width: 20%; height: 20%; border: none; padding: 0;margin: 0;"
+           src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDYvUc0kI&#x2F;YB6CZf30tS3X-4TuZ39AaA&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+         </iframe>
+       </div>
+       <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGDYvUc0kI&#x2F;YB6CZf30tS3X-4TuZ39AaA&#x2F;view?utm_content=DAGDYvUc0kI&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Abrir en canva</a>`,
+    },
+
+
+
+];
+
+tareas.forEach((prod) => {
+    const { id, nombre, fecha, desc, tarea } = prod;
+    if (contenedor) {
+        contenedor.innerHTML += `<div class contenido>
+        <div class="card mt-3 item">
+        <div id="content">
+        <div id="${id}">
+                <div id="${tarea}</div>
+            </div>
+
+            </div>
+        <div class="card-body">
+        <h3 class="card-title">${nombre}</h3>
+        <h4 class="card-text">Fecha: ${fecha}</h4>
+        <h4 class="card-text">${desc}</h4>
+      </div>
+    </div>
+    </div>
+                `;
+    }
+});
+
+/* tareas.forEach((prod) => {
+    const { id, nombre, fecha, tarea } = prod;
+    if (contenedor) {
+        contenedor.innerHTML += `
+    <div class="card mt-3 item">
+    <div id="content">
+    <div id="${id}">
+            <div id="${tarea}"</div>
+        </div>
+    <div class="card-body">
+    <h3 class="card-title">${nombre}</h3>
+    <h4 class="card-text">Fecha: ${fecha}</h4>
+</div>
+
+            
+  `;
+    }
+}); */
